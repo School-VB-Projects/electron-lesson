406e1ae9de7f21e3e60fa140ab14d6a94536a131
vbetsch <vic.betsch@gmail.com>
2023-05-02 12:04:13 +0200

    ✨ Choose Image

--- main.js
+++ main.js
@@ -1,38 +1,32 @@
-// require('update-electron-app')()
-const { app, BrowserWindow, ipcMain } = require('electron')
+const { app, BrowserWindow, ipcMain, dialog } = require('electron')
 const path = require('path')
 
-function handleSetTitle(event, title) {
-  const webContents = event.sender
-  const win = BrowserWindow.fromWebContents(webContents)
-  win.setTitle(title)
+async function handleFileOpen () {
+  const { canceled, filePaths } = await dialog.showOpenDialog()
+  if (canceled) {
+    return
+  } else {
+    return filePaths[0]
+  }
 }
 
-const createWindow = () => {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+function createWindow () {
+  const mainWindow = new BrowserWindow({
     webPreferences: {
-      preload: path.join(__dirname, 'scripts/preload.js'),
+      preload: path.join(__dirname, 'scripts/preload.js')
     }
   })
-
-  win.loadFile('index.html')
+  mainWindow.loadFile('index.html')
 }
 
 app.whenReady().then(() => {
-  ipcMain.on('set-title', handleSetTitle)
+  ipcMain.handle('dialog:openFile', handleFileOpen)
   createWindow()
-
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
-    }
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
 })
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
 })
--- scripts/renderer.js
+++ scripts/renderer.js
@@ -1,6 +1,9 @@
-const setButton = document.getElementById('btn')
-const titleInput = document.getElementById('title')
-setButton.addEventListener('click', () => {
-  const title = titleInput.value
-  window.electronAPI.setTitle(title)
+const btn = document.getElementById('btn')
+const filePathElement = document.getElementById('filePath')
+const img = document.getElementById('image')
+
+btn.addEventListener('click', async () => {
+  const filePath = await window.electronAPI.openFile()
+  filePathElement.innerText = filePath
+  img.src = filePath
 })
